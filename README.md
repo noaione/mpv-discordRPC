dff1d3c02d50366422c37598fcc0161a3a25a2a5
noaione <34302902+aimanmh@users.noreply.github.com>
2018-05-22 13:08:12 +0700

    Version 1.4
    Remaining time, more detailed info, and updated DiscordRPC.lua and .dll to version 3.3.0

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # mpv media player Discord RPC Integration
-#### This is work in progress thingymagic, since it can broke anytimes
+Using [lua-DiscordRPC](https://github.com/pfirsich/lua-discordRPC) as base and mpv lua module
+
+![Playing](https://image.ibb.co/dkjnt8/mpv_DRPC_Playing.png) ![Paused](https://image.ibb.co/mq496T/mpv_DRPC_Paused.png) ![Idling](https://image.ibb.co/eLdDY8/mpv_DRPC_Idling.png)
 
 ### [Download](https://github.com/noaione/mpv-discordRPC/releases)
+
+
 
 ### How to install (Windows)
 1. Download this repo or goto Release
@@ -19,14 +23,13 @@ echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.config/mpv/scripts/" >> ~/.
 
 ### Information:
 ```
--- Refresh interval: 15 seconds (?)
+-- Refresh interval: 15 seconds (Limited by Discord itself)
 -- Script: lua
--- Version: 1.3
+-- Current Version: 1.4
 ```
 
 ### Problem:
 ```
--- 15 seconds interval is too long
 -- Sometimes it won't gone after you exit (can be fixed by restarting discord: CTRL+R)
 -- Sometimes it takes about 10+ seconds until it show up
 ```
@@ -37,4 +40,5 @@ echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.config/mpv/scripts/" >> ~/.
 -- Version 1.1: Pause/Play kinda thingy, added win32/win64 version, linux/mac soon, since i don't know how lua loaded in linux/mac
 -- Version 1.2: Cleaned up, making idle mode more idle. Compacted everything to 3 files
 -- Version 1.3: Linux Support (please open issue if there is any problem)
+-- Version 1.4: Remaining time, more detailed info, and updated DiscordRPC.lua and .dll to version 3.3.0
 ```
--- linux/scripts/drpc.lua
+++ linux/scripts/drpc.lua
@@ -1,8 +1,9 @@
 local discordRPC = require("discordRPC")
 local appId = "441537954259664896" --Do not change this, or it will broke
+local count = 1
 
-function discordRPC.ready()
-	print("Discord: ready")
+function discordRPC.ready(userId, username, discriminator, avatar)
+    print(string.format("Discord: ready (%s, %s, %s, %s)", userId, username, discriminator, avatar))
 end
 
 function discordRPC.disconnected(errorCode, message)
@@ -13,28 +14,58 @@ function discordRPC.errored(errorCode, message)
     print(string.format("Discord: error (%d: %s)", errorCode, message))
 end
 
+function SecondsToClock(seconds)
+	local seconds = tonumber(seconds)
+
+	if seconds == nil then
+		return "00:00:00";
+	elseif seconds <= 0 then
+		return "00:00:00";
+	else
+		hours = string.format("%02.f", math.floor(seconds/3600));
+		mins = string.format("%02.f", math.floor(seconds/60 - (hours*60)));
+		secs = string.format("%02.f", math.floor(seconds - hours*3600 - mins *60));
+		return hours..":"..mins..":"..secs
+	end
+end
+
 local function discordrpc()
-	filename = mp.get_property("media-title")
-	filename = tostring(filename)
+	ff = mp.get_property("file-format")
+	ff = tostring(ff)
+	if ff == "mp3" or ff == "flac" or ff == "m4a" or ff == "wav" or ff == "dts" then
+		filename = mp.get_property("media-title")
+		filename = tostring(filename)
+		filename = filename.."."..ff
+	else
+		filename = mp.get_property("filename/no-ext")
+		filename = tostring(filename)
+	end
 	file = "file"
-	print(filename)
 	if filename == "nil" then
-		filename = "zzz~"
+		filename = "Idling"
 		file = "nofile"
 	end
-	print(durstr)
-	print(posstr)
+	
+	-- get time
+	timenow = os.time(os.date("*t"))
+	timest = os.time(os.date("*t", timest))
+	timeen = mp.get_property("playtime-remaining")
+	timeen = os.time(os.date("*t", timeen))
+	total = mp.get_property("duration")
+	total = SecondsToClock(total)
+	remain = SecondsToClock(timeen)
+	
 	-- get state
 	state = mp.get_property("core-idle")
 	state = tostring(state)
 	if state == "yes" then
 		stateimage = "pause"
-		detailstext = "Paused"
-		statetext = "Paused"
+		statetext = "Paused (" ..remain.. " Left)"
+		detailstext = "Paused (" ..total.. ")"
 	elseif state == "no" then
 		stateimage = "play"
 		statetext = "Playing"
-		detailstext = "Playing"
+		detailstext = "Playing (" ..total.. ")"
 	end
 	if file == "file" then
 		stateimage = stateimage
@@ -43,20 +74,43 @@ local function discordrpc()
 	elseif file == "nofile" then
 		stateimage = "idle"
 		statetext = "Idle"
-		detailstext = "Idling"
+		detailstext = ""
 	end
-	local now = os.time(os.date("*t"))
+	
 	-- init rpc and send
 	discordRPC.initialize(appId, true)
-	presence = {
-		state = detailstext,
-		details = filename,
-		largeImageKey = "mpvlogo",
-		largeImageText = "mpv Media player",
-		smallImageKey = stateimage,
-		smallImageText = statetext,
-	}
+	if state == "no" then
+		presence = {
+			state = detailstext,
+			details = filename,
+			startTimestamp = timenow,
+			endTimestamp = timenow + timeen,
+			largeImageKey = "mpvlogo",
+			largeImageText = "mpv Media player",
+			smallImageKey = stateimage,
+			smallImageText = statetext,
+		}
+	elseif state == "yes" or file == "nofile" then
+		presence = {
+			state = detailstext,
+			details = filename,
+			largeImageKey = "mpvlogo",
+			largeImageText = "mpv Media player",
+			smallImageKey = stateimage,
+			smallImageText = statetext,
+		}
+	end
+	nextPresenceUpdate = 0
+	
+	-- Pring Debug part (disable with double dash)
+	print("Now Playing: "..filename)
+	print("File Format: "..ff)
+	print("Total Time (In Seconds): "..total)
+	print("Current State: "..statetext)
+	
 	discordRPC.updatePresence(presence)
+	--discordRPC.updatePresence(presence)
 end
+
 
 mp.add_periodic_timer(1, discordrpc) -- set as 1 but repeat every 15 seconds(?)
